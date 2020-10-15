9cc87dfeb6fc0a2b426016fc6070558a0e79582d
Darragh Tobin <darraghtobin@gmail.com>
2020-10-15 23:44:02 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ElixirTutorial
+
+Access interactive elixir shell with `iex` command
+To exit iex use ctrl-c 
+Write script eg test.exs
+Run with `elixir test.exs`
---
+++ ch1/ch1.exs
@@ -0,0 +1,10 @@
+IO.puts rem 9,1
+:thisisatom
+byte_size("string")
+byte_size("longerString")
+byte_size(:atom)
+byte_size(:longerAtom)
+is_binary("Éoin")
+IO.puts [1, 2, 3, 4] ++ [1,1,1,1]
+add = fn a, b -> a + b end
+IO.puts add.(1, 2)
